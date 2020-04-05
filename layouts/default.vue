adb59f3620c5652362c2b085c3545b9a88737a2f
ykono <yfukyd@gmail.com>
2020-04-05 18:29:04 +0900

    change app var

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,44 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer expand-on-hover clipped fixed app>
+      <v-list-item>
+        <v-list-item-icon>
+          <img src="~/assets/mac.gif" />
+        </v-list-item-icon>
+        <v-list-item-title> Farming </v-list-item-title>
+      </v-list-item>
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in farmingMenu"
           :key="i"
           :to="item.to"
           router
           exact
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-img :src="item.imgsrc" />
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <img src="~/assets/hecate.gif" />
+        </v-list-item-icon>
+        <v-list-item-title> Boss </v-list-item-title>
+      </v-list-item>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in BossMenu"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-img :src="item.imgsrc" />
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
@@ -24,70 +46,44 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+    <v-app-bar clipped-left fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+
+    <v-footer fixed app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class DefaultLayout extends Vue {
+  drawer = false
+  farmingMenu = [
+    {
+      imgsrc: require('~/assets/hardgravity.gif'),
+      title: 'GRAVITY CRASH',
+      to: '/hardgravity'
     }
-  }
+  ]
+
+  BossMenu = [
+    {
+      imgsrc: require('~/assets/hellfulks.gif'),
+      title: 'SCYTHE',
+      to: '/scythe'
+    }
+  ]
+
+  title = 'PandaTo Damage calculator'
 }
 </script>
