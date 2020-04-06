8580296c6251386490685a2e2afe583fce83fa26
ykono <yfukyd@gmail.com>
2020-04-06 21:18:20 +0900

    add sub list

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,74 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer expand-on-hover permanent clipped fixed app>
+    <v-navigation-drawer
+      expand-on-hover
+      clipped
+      fixed
+      app
+      permanent
+      :mini-variant.sync="minivaliant"
+      width="300"
+    >
       <v-list-item>
         <v-list-item-icon>
           <img src="~/assets/mac.gif" />
         </v-list-item-icon>
         <v-list-item-title> Farming </v-list-item-title>
       </v-list-item>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in farmingMenu"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-img :src="item.imgsrc" />
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <v-expansion-panels v-model="farmingPanel" multiple accordion>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div>
+              <img src="~/assets/attacktype.jpg" />
+              <span :hidden="minivaliant">Attack Type</span>
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in farmingMenu"
+                :key="i"
+                :to="item.to"
+                router
+                exact
+              >
+                <v-list-item-action>
+                  <v-img :src="item.imgsrc" />
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div>
+              <img src="~/assets/magictype.jpg" />
+              <span :hidden="minivaliant">Magic Type</span>
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in farmingMenu"
+                :key="i"
+                :to="item.to"
+                router
+                exact
+              >
+                <v-list-item-action>
+                  <v-img :src="item.imgsrc" />
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
       <v-list-item>
         <v-list-item-icon>
           <img src="~/assets/hecate.gif" />
@@ -67,27 +113,53 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class DefaultLayout extends Vue {
-  drawer = false
+  drawer = true
+  farmingPanel = [0]
+  minivaliant = false
   farmingMenu = [
     {
-      imgsrc: require('~/assets/earthquakeblade.gif'),
-      title: 'EARTHQUAKE BLADE',
-      to: '/earthquakeblade'
+      imgsrc: require('~/assets/attacktype.jpg'),
+      title: 'ATTACK TYPE',
+      skills: [
+        {
+          imgsrc: require('~/assets/earthquakeblade.gif'),
+          title: 'EARTHQUAKE BLADE',
+          to: '/earthquakeblade'
+        }
+      ]
     },
     {
-      imgsrc: require('~/assets/hardgravity.gif'),
-      title: 'GRAVITY CRASH',
-      to: '/gravitycrash'
+      imgsrc: require('~/assets/magictype.jpg'),
+      title: 'MAGIC TYPE',
+      skills: [
+        {
+          imgsrc: require('~/assets/hardgravity.gif'),
+          title: 'GRAVITY CRASH',
+          to: '/gravitycrash'
+        }
+      ]
     },
     {
-      imgsrc: require('~/assets/fanofknives.gif'),
-      title: 'FAN OF KNIVES',
-      to: '/fanofknives'
+      imgsrc: require('~/assets/attacktype.jpg'),
+      title: 'SENSE TYPE',
+      skills: [
+        {
+          imgsrc: require('~/assets/fanofknives.gif'),
+          title: 'FAN OF KNIVES',
+          to: '/fanofknives'
+        }
+      ]
     },
     {
-      imgsrc: require('~/assets/fullhouse.gif'),
-      title: 'FULL HOUSE',
-      to: '/fullhouse'
+      imgsrc: require('~/assets/attacktype.jpg'),
+      title: 'CHARM TYPE',
+      skills: [
+        {
+          imgsrc: require('~/assets/fullhouse.gif'),
+          title: 'FULL HOUSE',
+          to: '/fullhouse'
+        }
+      ]
     }
   ]
 
