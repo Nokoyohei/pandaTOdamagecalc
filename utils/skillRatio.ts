3f629cb1d6b65a126972f64a9bd9beafa7d58d51
ykono <yfukyd@gmail.com>
2020-04-28 08:14:34 +0900

    Follow the Earthquake Blade nerf

--- utils/skillRatio.ts
+++ utils/skillRatio.ts
@@ -1,7 +1,7 @@
 const DarkCommando = 300 / 10
 const GravityCrash = 1200 / 10
 const FullHouse = 800 / 100
-const EarthquakeBlade = (attr: number) => 230 / 100 + (attr * 2) / 100
+const EarthquakeBlade = (attr: number) => 190 / 100 + (attr * 2) / 100
 const ChampionsBlade = (attr: number) => 400 / 100 + attr / 100
 const FanOfKnives = 800 / 10
 const ShootingSpree = 400 / 100
