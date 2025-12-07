d6abfd04d63020aac7691c6d9d5d2dc3a914f8c8
Nokoyohei <yfukyd@gmail.com>
2025-12-07 11:46:09 +0900

    tmp

--- utils/buffRatio.ts
+++ utils/buffRatio.ts
@@ -1,16 +1,16 @@
-const MistOfManaBuff = 0.5
-export const BloodTestamentBuff = 0.2
-const PumpingHeartBuff = 0.3
-const AdrenalineBuff = 0.3
-const LuckySevenBuff = 0.5
-const AuraOfLuckBuff = 0.4
+const MistOfManaBuff = 0.45
+export const BloodTestamentBuff = 0.15
+const PumpingHeartBuff = 0.16
+const AdrenalineBuff = 0.26
+const LuckySevenBuff = 0.45
+const AuraOfLuckBuff = 0.18
 const FirstdAnniversaryBuff = 0.1
-const DodgeMasterBuff = 0.6
-const SixthSenseBuff = 0.65
-const secondAnniversaryBuff = 0.2
-const GunBoosterBuff = 0.7
-const BullsEyeBuff = 0.7
-const EagleEyeBuff = 0.5
+const DodgeMasterBuff = 0.45
+const SixthSenseBuff = 0.55
+const secondAnniversaryBuff = 0.1
+const GunBoosterBuff = 0.45
+const BullsEyeBuff = 0.45
+const EagleEyeBuff = 0.45
 const PrecisePitch = 10
 
 export default {
--- utils/skillRatio.ts
+++ utils/skillRatio.ts
@@ -1,45 +1,45 @@
-const DarkCommando = 300 / 10
-const GravityCrash = 1200 / 10
+const DarkCommando = 186 / 10
+const GravityCrash = 900 / 10
 const CelestialStrike = (skillNum: number) =>
-  (950 / 10) * (1.5 + 0.05 * skillNum)
-const Scythe = (attr: number) => 55 + (30 * attr) / 100
-const StaffOfAgony = (attr: number) => 85 + (15 * attr) / 100
+  (550 / 10) * (1.5 + 0.05 * skillNum)
+const Scythe = (attr: number) =>28.5 + (30 * attr / 100) //
+const StaffOfAgony = (attr: number) => 48 + (15 * attr) / 100 //
 const FullHouse = 800 / 100
-const EarthquakeBlade = (attr: number) => 190 / 100 + (attr * 2) / 100
-const SonicSlash = (attr: number) => 700 / 100 + (attr * 3.5) / 100
-const TidalSlash = (attr: number) => 500 / 100 + (attr * 2.5) / 100
-const TempestStrike = (attr: number) => 280 / 100 + (attr * 5) / 100
-const GaleStrike = (attr: number) => 300 / 100 + (attr * 7) / 100
-const ChampionsBlade = (attr: number) => 400 / 100 + attr / 100
-const FlamingFist = (attr: number) => 40 / 100 + attr / 100
-const FanOfKnives = 1000 / 10
-const ChainOfKnives = 950 / 100
-const SuddenAttack = 1600 / 100
-const PoisonAssault = 800 / 100
-const LuckyFist = 380 / 1000
-const ShootingSpree = 400 / 100
-const Berserk = 550 / 100
-const PowerShot = 800 / 100
-const DoubleShot = 1000 / 100
-const FireBlessing = 1000 / 20
-const WaterBlessing = 1000 / 20
-const EarthBlessing = 65 / 20
-const ElecBlessing = 65 / 20
-const WindBlessing = 65 / 20
-const WindBlade = 1200 / 10
-const RasingStorm = 700 / 10
-const ElectroAttack = 1150 / 10
-const StaffOfThunder = 1600 / 10
-const TeslaField = 800 / 10
-const DeadlyFen = 1000 / 20
-const Earthquake = 1500 / 10
-const TornadoBlast = 1100 / 10
-const CleavingTerra = 1100 / 10
-const SharpScream = (800 / 200) * 1.5
-const FirstHitCombo = 800 / 300
-const SecondHitCombo = 800 / 400
-const OnePair = 1200 / 100
-const LadyLuck = 6 / 15
+const EarthquakeBlade = (attr: number) => 440 / 100 + (attr * 2) / 100
+const SonicSlash = (attr: number) => 700 / 100 + (attr * 3.5) / 100 //
+const TidalSlash = (attr: number) => 500 / 100 + (attr * 2.5) / 100 //
+const TempestStrike = (attr: number) => 280 / 100 + (attr * 5) / 100 //
+const GaleStrike = (attr: number) => 300 / 100 + (attr * 7) / 100 //
+const ChampionsBlade = (attr: number) => 490 / 100 + attr / 100
+const FlamingFist = (attr: number) => 570 / 100 + attr / 100
+const FanOfKnives = 1000 / 10 //
+const ChainOfKnives = 950 / 100 //
+const SuddenAttack = 1600 / 100 //
+const PoisonAssault = 800 / 100 //
+const LuckyFist = 440 / 1000 
+const ShootingSpree = 400 / 100 //
+const Berserk = 550 / 100 //
+const PowerShot = 800 / 100 //
+const DoubleShot = 1000 / 100 //
+const FireBlessing = 1000 / 20 //
+const WaterBlessing = 1000 / 20 //
+const EarthBlessing = 65 / 20 //
+const ElecBlessing = 65 / 20 //
+const WindBlessing = 65 / 20 //
+const WindBlade = 268 / 10
+const RasingStorm = 700 / 10 //
+const ElectroAttack = 1150 / 10 //
+const StaffOfThunder = 1600 / 10 //
+const TeslaField = 800 / 10 //
+const DeadlyFen = 1000 / 20 //
+const Earthquake = 1500 / 10 //
+const TornadoBlast = 1100 / 10 //
+const CleavingTerra = 337 / 10
+const SharpScream = (800 / 200) * 1.5 //
+const FirstHitCombo = 800 / 300 //
+const SecondHitCombo = 800 / 400 //
+const OnePair = 1200 / 100 //
+const LadyLuck = 6 / 15 //
 
 export default {
   DarkCommando,
